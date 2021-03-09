b233b239c7172a527b21ab52e44db2cd09f052f3
ddialar <ddialar@gmail.com>
2021-03-09 17:19:02 +0000

    chore(repository): Initialize the repository.

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {ApplicationConfig, Main} from './application';
+
+export * from './application';
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new Main(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  // REFACTOR Replace this log for a specific logger suite
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
+
+if (require.main === module) {
+  const config = {
+    rest: {
+      port: +(process.env.PORT ?? 3000),
+      host: process.env.HOST,
+      gracePeriodForClose: 5000,
+      openApiSpec: {
+        setServersFromRequest: true,
+      },
+    },
+  };
+  main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
+}

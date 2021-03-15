44e71af4bef38da159b705261e23b8f61df55df9
ddialar <ddialar@gmail.com>
2021-03-15 13:47:51 +0000

    feat(services): Implement 'MyUserService'.

---
+++ src/services/user.service.ts
@@ -0,0 +1,52 @@
+import {UserService} from '@loopback/authentication';
+import {BindingScope, injectable} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {HttpErrors} from '@loopback/rest';
+import {securityId, UserProfile} from '@loopback/security';
+import {LoginInputParams} from '../controllers';
+import {User} from '../models';
+import {UserRepository} from '../repositories';
+import {HashService} from './hash.service';
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class MyUserService implements UserService<User, LoginInputParams> {
+  constructor(
+    @repository(UserRepository) public userRepository: UserRepository,
+  ) {}
+
+  async verifyCredentials({
+    username,
+    password,
+  }: LoginInputParams): Promise<User> {
+    const invalidCredentialsError = 'Invalid username or password';
+
+    const retrievedUser = await this.userRepository.findOne({
+      where: {username},
+    });
+    if (!retrievedUser) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    const {password: hashedPassword} = retrievedUser;
+
+    const validPassword = await HashService.checkPassword(
+      password,
+      hashedPassword,
+    );
+    if (!validPassword) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    return retrievedUser;
+  }
+
+  convertToUserProfile({id, name, surname, username}: User): UserProfile {
+    return {
+      [securityId]: id!,
+      id,
+      name,
+      surname,
+      username,
+    };
+  }
+}

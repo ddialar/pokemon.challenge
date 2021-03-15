c9e340c555d8d7378e765d96d9c5276370cf7a91
ddialar <ddialar@gmail.com>
2021-03-15 22:06:28 +0000

    refactor(services): Improve the authentication service.

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,9 +1,11 @@
 import {TokenService, UserService} from '@loopback/authentication';
 import {BindingScope, injectable, service} from '@loopback/core';
+import {HttpErrors} from '@loopback/rest';
 import {LoginInputParams} from '../types';
 import {User} from './../models/user.model';
 import {JwtService} from './jwt.service';
 import {MyUserService} from './user.service';
+import {ValidatorService} from './validator.service';
 
 export interface AuthenticatedUser {
   token: string;
@@ -13,18 +15,31 @@ export interface AuthenticatedUser {
 export class AuthenticationService {
   constructor(
     @service(JwtService)
-    public jwtService: TokenService,
+    private jwtService: TokenService,
     @service(MyUserService)
-    public userService: UserService<User, LoginInputParams>,
+    private userService: UserService<User, LoginInputParams>,
+    @service(ValidatorService)
+    private validationService: ValidatorService,
   ) {}
 
   public async login(
     username: string,
     password: string,
   ): Promise<AuthenticatedUser> {
-    const user = await this.userService.verifyCredentials({username, password});
-    const userProfile = this.userService.convertToUserProfile(user);
-    const token = await this.jwtService.generateToken(userProfile);
-    return {token};
+    this.validationService.validateLogin({username, password});
+
+    try {
+      const user = await this.userService.verifyCredentials({
+        username,
+        password,
+      });
+      const userProfile = this.userService.convertToUserProfile(user);
+      const token = await this.jwtService.generateToken(userProfile);
+      return {token};
+    } catch ({message}) {
+      throw new HttpErrors.InternalServerError(
+        `Login process error: ${message}`,
+      );
+    }
   }
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
 export * from './authentication.service';
 export * from './jwt.service';
 export * from './user.service';
+export * from './pokemon.service';
+export * from './validator.service';

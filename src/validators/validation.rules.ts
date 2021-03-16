9e7b798234f01b8972bab9654e11c887c9862c37
ddialar <ddialar@gmail.com>
2021-03-16 21:49:10 +0000

    fix(validator): Edit the 'filterLimit' and 'filterPage' minimum values.

--- src/validators/validation.rules.ts
+++ src/validators/validation.rules.ts
@@ -10,10 +10,6 @@ export const pokemonNameRequired = pokemonName.required();
 export const pokemonType = Joi.string().pattern(
   new RegExp(process.env.POKEMON_TYPE_PATTERN!),
 );
-export const filterPage = Joi.number().min(
-  parseInt(process.env.POKEMON_FILTER_DEFAULT_PAGE!, 10),
-);
-export const filterLimit = Joi.number().min(
-  parseInt(process.env.POKEMON_FILTER_DEFAULT_LIMIT!, 10),
-);
+export const filterPage = Joi.number().min(1);
+export const filterLimit = Joi.number().min(0);
 export const filterFavorite = Joi.boolean();

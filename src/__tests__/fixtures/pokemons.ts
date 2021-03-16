363224553be7eb6e457eebc8c83a54f0faffb4c9
ddialar <ddialar@gmail.com>
2021-03-16 23:12:24 +0000

    chore(fixtures): Add new pokemon fixtures.

--- src/__tests__/fixtures/pokemons.ts
+++ src/__tests__/fixtures/pokemons.ts
@@ -1,5 +1,7 @@
 import mockedPokemons from './assets/pokemons.json';
 
 export const testingPokemons = mockedPokemons;
+export const testingPokemonsAmount = testingPokemons.length;
 export const testingNonPersistedPokemonId = '999';
 export const testingNonPersistedPokemonName = 'TestingPokemonName';
+export const testingNonPersistedPokemonType = 'TestingPokemonType';

4bdb1ee576c8d9a62f4853150d5a212fa91a5bdd
ddialar <ddialar@gmail.com>
2021-03-16 15:37:41 +0000

    feat(controllers): Implement several methods in the pokemon controller and replace the response and attribute schemas by constants.

--- src/controllers/pokemon/pokemon.controller.ts
+++ src/controllers/pokemon/pokemon.controller.ts
@@ -1,28 +1,37 @@
-import {repository} from '@loopback/repository';
-import {get, param, response} from '@loopback/rest';
+import {service} from '@loopback/core';
+import {get, param, put, response} from '@loopback/rest';
 import {Pokemon} from '../../models';
 import {PokemonService} from './../../services/pokemon.service';
 import {
   POKEMONS_ARRAY_RESPONSE_SCHEMA,
+  POKEMON_FILTER_FAVORITE_SCHEMA,
+  POKEMON_FILTER_LIMIT_SCHEMA,
+  POKEMON_FILTER_NAME_SCHEMA,
+  POKEMON_FILTER_PAGE_SCHEMA,
+  POKEMON_FILTER_TYPE_SCHEMA,
+  POKEMON_ID_SCHEMA,
+  POKEMON_MARKED_FAVORITE_SUCCESS_SCHEMA,
   POKEMON_TYPES_ARRAY_RESPONSE_SCHEMA,
+  POKEMON_UNMARKED_FAVORITE_SUCCESS_SCHEMA,
   SINGLE_POKEMON_RESPONSE_SCHEMA,
 } from './pokemon.schemas';
 
 export class PokemonController {
   constructor(
-    @repository(PokemonService)
+    @service(PokemonService)
     public pokemonService: PokemonService,
   ) {}
 
   @get('/pokemons')
   @response(200, POKEMONS_ARRAY_RESPONSE_SCHEMA)
   async find(
-    @param.query.string('name') name?: string,
-    @param.query.string('type') type?: string,
-    @param.query.integer('page') page?: number,
-    @param.query.integer('limit') limit?: number,
+    @param(POKEMON_FILTER_NAME_SCHEMA) name?: string,
+    @param(POKEMON_FILTER_TYPE_SCHEMA) type?: string,
+    @param(POKEMON_FILTER_PAGE_SCHEMA) page?: number,
+    @param(POKEMON_FILTER_LIMIT_SCHEMA) limit?: number,
+    @param(POKEMON_FILTER_FAVORITE_SCHEMA) favorite?: boolean,
   ): Promise<Pokemon[]> {
-    return this.pokemonService.getFiltered({name, type, page, limit});
+    return this.pokemonService.getFiltered({name, type, page, limit, favorite});
   }
 
   @get('/pokemons/{id}')
@@ -41,5 +50,17 @@ export class PokemonController {
   @response(200, POKEMON_TYPES_ARRAY_RESPONSE_SCHEMA)
   async findTypes(): Promise<string[]> {
     return this.pokemonService.getTypes();
+  }
+
+  @put('/pokemons/markAsFavorite/{id}')
+  @response(204, POKEMON_MARKED_FAVORITE_SUCCESS_SCHEMA)
+  async markAsFavorite(@param(POKEMON_ID_SCHEMA) id: string): Promise<void> {
+    await this.pokemonService.markAsFavorite(id);
+  }
+
+  @put('/pokemons/unmarkAsFavorite/{id}')
+  @response(204, POKEMON_UNMARKED_FAVORITE_SUCCESS_SCHEMA)
+  async unmarkAsFavorite(@param(POKEMON_ID_SCHEMA) id: string): Promise<void> {
+    await this.pokemonService.unmarkAsFavorite(id);
   }
 }
--- src/controllers/pokemon/pokemon.schemas.ts
+++ src/controllers/pokemon/pokemon.schemas.ts
@@ -1,4 +1,4 @@
-import {ResponseModelOrSpec} from '@loopback/openapi-v3';
+import {ParameterObject, ResponseModelOrSpec} from '@loopback/openapi-v3';
 import {getModelSchemaRef} from '@loopback/rest';
 import {Pokemon} from './../../models/pokemon/pokemon.model';
 
@@ -34,5 +34,70 @@ export const SINGLE_POKEMON_RESPONSE_SCHEMA: ResponseModelOrSpec = {
     'application/json': {
       schema: getModelSchemaRef(Pokemon, {includeRelations: true}),
     },
+  },
+};
+
+export const POKEMON_MARKED_FAVORITE_SUCCESS_SCHEMA: ResponseModelOrSpec = {
+  description: 'Pokemon marked as favorite successfully',
+};
+
+export const POKEMON_UNMARKED_FAVORITE_SUCCESS_SCHEMA: ResponseModelOrSpec = {
+  description: 'Pokemon unmarked as favorite successfully',
+};
+
+export const POKEMON_ID_SCHEMA: ParameterObject = {
+  name: 'id',
+  in: 'path',
+  required: true,
+  schema: {
+    type: 'string',
+    pattern: process.env.POKEMON_ID_PATTERN,
+  },
+};
+
+export const POKEMON_FILTER_NAME_SCHEMA: ParameterObject = {
+  name: 'name',
+  in: 'query',
+  schema: {
+    type: 'string',
+    pattern: process.env.POKEMON_NAME_PATTERN,
+  },
+};
+
+export const POKEMON_FILTER_TYPE_SCHEMA: ParameterObject = {
+  name: 'type',
+  in: 'query',
+  schema: {
+    type: 'string',
+    pattern: process.env.POKEMON_TYPE_PATTERN,
+  },
+};
+
+export const POKEMON_FILTER_PAGE_SCHEMA: ParameterObject = {
+  name: 'page',
+  in: 'query',
+  schema: {
+    type: 'number',
+    minimum: parseInt(process.env.POKEMON_FILTER_DEFAULT_PAGE!, 10),
+  },
+};
+
+export const POKEMON_FILTER_LIMIT_SCHEMA: ParameterObject = {
+  name: 'limit',
+  in: 'query',
+  schema: {
+    type: 'number',
+    minimum: 0,
+    default: parseInt(process.env.POKEMON_FILTER_DEFAULT_LIMIT!, 10),
+    description:
+      'If this value is set to 0, the whole records will be returned',
+  },
+};
+
+export const POKEMON_FILTER_FAVORITE_SCHEMA: ParameterObject = {
+  name: 'favorite',
+  in: 'query',
+  schema: {
+    type: 'boolean',
   },
 };

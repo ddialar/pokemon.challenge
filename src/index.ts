b34fdaa7c94c4e0c95507bfc4ec444eccb03b4a8
ddialar <ddialar@gmail.com>
2021-03-16 15:26:49 +0000

    feat(index): Implement the 'checkStartup' method.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import {ApplicationConfig, Main} from './application';
 export * from './application';
 
 export async function main(options: ApplicationConfig = {}) {
+  checkStartup();
+
   const app = new Main(options);
   await app.boot();
   await app.start();
@@ -30,4 +32,37 @@ if (require.main === module) {
     console.error('Cannot start the application.', err);
     process.exit(1);
   });
+}
+
+function checkStartup() {
+  const requiredEnvVariables = [
+    'MONGO_USER',
+    'MONGO_PASS',
+    'MONGO_HOST',
+    'MONGO_PORT',
+    'MONGO_DB',
+    'POKEMON_ID_PATTERN',
+    'POKEMON_NAME_PATTERN',
+    'POKEMON_TYPE_PATTERN',
+    'POKEMON_FILTER_DEFAULT_PAGE',
+    'POKEMON_FILTER_DEFAULT_LIMIT',
+  ];
+
+  const nonConfiguredVariables = requiredEnvVariables.reduce(
+    (result: string[], variable) => {
+      return process.env[variable] ? result : [...result, variable];
+    },
+    [],
+  );
+
+  if (nonConfiguredVariables.length) {
+    console.error(
+      `The next required environment variables are not configured: ${JSON.stringify(
+        nonConfiguredVariables,
+      )}`,
+    );
+    process.exit(1);
+  } else {
+    console.log('Environment variables successfully configured');
+  }
 }

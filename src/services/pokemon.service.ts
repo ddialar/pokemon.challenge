5c52b8401470938f3d5f46e3185977e259465ae1
ddialar <ddialar@gmail.com>
2021-03-16 15:39:43 +0000

    feat(services): Implement several methods in the pokemon service and decoupling other ones from the repository.

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,16 +1,10 @@
 import {BindingScope, injectable, service} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {HttpErrors} from '@loopback/rest';
+import {PokemonRequestFilter} from '../types';
 import {Pokemon} from './../models';
 import {PokemonRepository} from './../repositories/pokemon.repository';
 import {ValidatorService} from './validator.service';
-
-export type PokemonRequestFilter = {
-  name?: string;
-  type?: string;
-  page?: number;
-  limit?: number;
-};
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class PokemonService {
@@ -34,19 +28,10 @@ export class PokemonService {
   }
 
   async getByName(name: string): Promise<Pokemon> {
-    this.validationService.validatePokemonName(name);
-
-    const nameRegex = new RegExp(name, 'i');
-    const filter = {
-      where: {
-        name: {
-          regexp: nameRegex,
-        },
-      },
-    };
+    this.validationService.validatePokemonName(name, true);
 
     try {
-      const retrievedPokemon = await this.pokemonRepository.findOne(filter);
+      const retrievedPokemon = await this.pokemonRepository.findByName(name);
 
       if (!retrievedPokemon) {
         throw new HttpErrors.NotFound();
@@ -63,44 +48,89 @@ export class PokemonService {
   }
 
   async getTypes(): Promise<string[]> {
-    const retrievedTypes = await this.pokemonRepository.find(
-      {},
-      {fields: {types: true}},
-    );
+    try {
+      const retrievedTypes = await this.pokemonRepository.findTypesPerPokemon();
 
-    return retrievedTypes
-      .reduce((result, {types}) => {
-        types.forEach(type => {
-          result = result.includes(type) ? result : [...result, type];
-        });
-        return result;
-      }, [] as string[])
-      .sort();
+      return retrievedTypes
+        .reduce((result, types) => {
+          types.forEach(type => {
+            result = result.includes(type) ? result : [...result, type];
+          });
+          return result;
+        }, [] as string[])
+        .sort();
+    } catch ({message}) {
+      throw new HttpErrors.InternalServerError(
+        `Retrieving pokemon types: ${message}`,
+      );
+    }
   }
 
   async getFiltered({
     name,
     type,
-    page,
-    limit,
+    page = parseInt(process.env.POKEMON_FILTER_DEFAULT_PAGE!, 10),
+    limit = parseInt(process.env.POKEMON_FILTER_DEFAULT_LIMIT!, 10),
+    favorite,
   }: PokemonRequestFilter): Promise<Pokemon[]> {
-    const where = {
-      name: name && {regexp: new RegExp(name, 'i')},
-      types: type ? {inq: [[type]]} : undefined,
-    };
-    const filter = {
-      where,
-      skip: page && limit ? (page - 1) * limit : undefined,
-      limit,
-      order: ['id ASC'],
-    };
+    this.validationService.validatePokemonName(name);
+    this.validationService.validatePokemonType(type);
+    this.validationService.validateFilterPage(page);
+    this.validationService.validateFilterLimit(limit);
+    this.validationService.validateFilterFavorite(favorite);
 
     try {
-      return await this.pokemonRepository.findFiltered(filter);
+      return await this.pokemonRepository.findFiltered({
+        name,
+        type,
+        page,
+        limit,
+        favorite,
+      });
     } catch ({message}) {
       throw new HttpErrors.InternalServerError(
         `Retrieving pokemon using filtering parameters: ${message}`,
       );
+    }
+  }
+
+  async markAsFavorite(id: string): Promise<void> {
+    this.validationService.validatePokemonId(id);
+
+    try {
+      const retrievedPokemon = await this.pokemonRepository.findById(id);
+
+      if (!retrievedPokemon) {
+        throw new HttpErrors.NotFound();
+      }
+
+      await this.pokemonRepository.markAsFavorite(retrievedPokemon);
+    } catch (error) {
+      throw error.isHttpError
+        ? error
+        : new HttpErrors.InternalServerError(
+            `Marking pokemon '${id}' as favorite: ${error.message}`,
+          );
+    }
+  }
+
+  async unmarkAsFavorite(id: string): Promise<void> {
+    this.validationService.validatePokemonId(id);
+
+    try {
+      const retrievedPokemon = await this.pokemonRepository.findById(id);
+
+      if (!retrievedPokemon) {
+        throw new HttpErrors.NotFound();
+      }
+
+      await this.pokemonRepository.unmarkAsFavorite(retrievedPokemon);
+    } catch (error) {
+      throw error.isHttpError
+        ? error
+        : new HttpErrors.InternalServerError(
+            `Unmarking pokemon '${id}' as favorite: ${error.message}`,
+          );
     }
   }
 }
